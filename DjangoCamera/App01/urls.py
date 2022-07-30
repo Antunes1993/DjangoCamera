7e56845677aecdb78bfd5ede04d92d2faba75a37
Antunes <feoxp7@splm.siemens.com>
2022-07-30 16:40:48 -0300

    Config Heroku

--- DjangoCamera/App01/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from .views import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('video_feed', views.video_feed, name='video_feed'),
-    path('webcam_feed', views.webcam_feed, name='webcam_feed'),
-]
